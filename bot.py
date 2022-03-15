d857f92646002a01b2c57c09b7c9a2aac9b65603
Ebaleiz <101637738+Ebaleiz@users.noreply.github.com>
2022-03-15 18:43:39 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,14 @@ def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-updater = Updater('51102:AAFcZLSungEdAm-VPMnkt5WkYtsq5JN9ccI')
+updater = Updater('5110232242:AAFcZLSungEdAm-VPMnkt5WkYtsq5JN9ccI')
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+def sent_messages(update, context):
+    chat_id = update.effective_chat.id
+    update.message.reply_text("start")
+    print("start called from chat with id = {}".format(chat_id))
 
 updater.start_polling()
 updater.idle()
